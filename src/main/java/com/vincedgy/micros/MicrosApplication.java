e0c3b2f8f8dff41d24af85408cdc134f895832bc
Vincent <vincent.dagoury@gmail.com>
2020-06-20 12:38:34 +0200

    First commit

---
+++ src/main/java/com/vincedgy/micros/MicrosApplication.java
@@ -0,0 +1,13 @@
+package com.vincedgy.micros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosApplication.class, args);
+	}
+
+}
